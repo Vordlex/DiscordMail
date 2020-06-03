c49155834b668daa56f3b44b6c6f681131490f63
Yago <yago.ladislau@tksolucoes.com.br>
2020-06-03 16:02:28 -0300

    Começo com o teste dos rastreios

--- src/commands/TrackCMD.js
+++ src/commands/TrackCMD.js
@@ -1,5 +1,15 @@
-const TrackCMD = () => {
-  
+const { RastreioBrasil } = require("correios-brasil")
+
+const correios = new RastreioBrasil()
+const codRastreio = ["PY360109365BR"]
+
+const TrackCMD = async () => {
+  try {
+    const resp = await correios.rastrearEncomendas(codRastreio)
+    console.log(resp[0])
+  } catch (error) {
+    console.log(error)
+  }
 }
 TrackCMD()
 module.export = TrackCMD
